b374fe32c9673f12931e27bebf3d99f381ca9ff7
Henry Abayan <h.abayan@yahoo.com>
2025-06-10 14:50:41 +0800

    Add Seeder for Grade, Quarter, Proficiency Level and Language

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,60 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Administrator',
-            'email' => 'admin@mail.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Administrator',
+        //     'email' => 'admin@mail.com',
+        // ]);
+
+        // Grade Seeder
+
+        $grades = [
+            ['level' => 'Grade 1', 'active' => true],
+            ['level' => 'Grade 2', 'active' => true],
+            ['level' => 'Grade 3', 'active' => true],
+            ['level' => 'Grade 4', 'active' => true],
+            ['level' => 'Grade 5', 'active' => true],
+            ['level' => 'Grade 6', 'active' => true],
+            ['level' => 'Grade 7', 'active' => true],
+            ['level' => 'Grade 8', 'active' => true],
+            ['level' => 'Grade 9', 'active' => true],
+        ];
+
+        foreach ($grades as $grade) {
+            \App\Models\Grade::create($grade);
+        }
+
+        $quarters = [
+            ['quarter' => 'First Quarter', 'active' => true],
+            ['quarter' => 'Second Quarter', 'active' => true],
+            ['quarter' => 'Third Quarter', 'active' => true],
+            ['quarter' => 'Fourth Quarter', 'active' => true],
+        ];
+
+        foreach ($quarters as $quarter) {
+            \App\Models\Quarter::create($quarter);
+        }
+
+        $proficiencies = [
+            ['level' => 'Novice', 'active' => true],
+            ['level' => 'Expert', 'active' => true],
+            ['level' => 'Master', 'active' => true],
+        ];
+
+        foreach ($proficiencies as $proficiency) {
+            \App\Models\ProficiencyLevel::create($proficiency);
+        }
+
+        $languages = [
+            ['language' => 'English', 'active' => true],
+            ['language' => 'Filipino', 'active' => true],
+            ['language' => 'Cebuano', 'active' => true],
+            ['language' => 'Ilocano', 'active' => true],
+            ['language' => 'Hiligaynon', 'active' => true],
+        ];
+        
+        foreach ($languages as $language) {
+            \App\Models\Language::create($language);
+        }
     }
 }
