919464c0c9bfa7033894c2d3812737bd22d385f9
Nikko Ador <nikkoador@Nikkos-MacBook-Pro.local>
2025-06-26 21:36:28 +0800

    Change GPT Version

--- config/openai.php
+++ config/openai.php
@@ -47,8 +47,8 @@ return [
 
     'request_timeout' => env('OPENAI_REQUEST_TIMEOUT', 30),
     // 'assessment_tool_model' => 'gpt-3.5-turbo-0125',
-    'assessment_tool_model' => 'gpt-3.5-turbo',
+    'assessment_tool_model' => 'gpt-4.1-mini',
     // 'lesson_planner_model' => 'gpt-3.5-turbo-1106',
-    // 'lesson_planner_model' => 'gpt-4.1-mini',
-    'lesson_planner_model' => 'gpt-4.1',
+    'lesson_planner_model' => 'gpt-4.1-mini',
+    // 'lesson_planner_model' => 'gpt-4.1',
 ];
